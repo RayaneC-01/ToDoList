6eb60226a9a649c11a1ef794148b437197b41dfc
Rayane <rchaabane20@gmail.com>
2024-05-14 00:32:40 +0200

    Fichier JS

--- script.js
+++ script.js
@@ -42,3 +42,113 @@ function CreateToDoItems() {
     todoValue.value = "";
     setAlertMessage("Todo item Created Successfully!");
 }
+
+function ReadToDoItems() {
+    todo.forEach((element) => {
+        let li = document.createElement("li");
+        let style = "";
+        if (element.status) {
+            style = "style='text-decoration: line-through'";
+        }
+        const todoItems = `<div ${style} title="Hit Double Click and Complete" ondblclick="CompletedToDoItems(this)">${element.item
+            }
+      ${style === ""
+                ? ""
+                : '<img class="todo-controls" src="/images/check-mark.png" />'
+            }</div><div>
+      ${style === ""
+                ? '<img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="/images/pencil.png" />'
+                : ""
+            }
+      <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="/images/delete.png" /></div></div>`;
+        li.innerHTML = todoItems;
+        listItems.appendChild(li);
+    });
+}
+ReadToDoItems();
+
+function UpdateToDoItems(e) {
+    if (
+        e.parentElement.parentElement.querySelector("div").style.textDecoration ===
+        ""
+    ) {
+        todoValue.value =
+            e.parentElement.parentElement.querySelector("div").innerText;
+        updateText = e.parentElement.parentElement.querySelector("div");
+        addUpdate.setAttribute("onclick", "UpdateOnSelectionItems()");
+        addUpdate.setAttribute("src", "/images/refresh.png");
+        todoValue.focus();
+    }
+}
+
+function UpdateOnSelectionItems() {
+    let IsPresent = false;
+    todo.forEach((element) => {
+        if (element.item == todoValue.value) {
+            IsPresent = true;
+        }
+    });
+
+    if (IsPresent) {
+        setAlertMessage("This item already present in the list!");
+        return;
+    }
+
+    todo.forEach((element) => {
+        if (element.item == updateText.innerText.trim()) {
+            element.item = todoValue.value;
+        }
+    });
+    setLocalStorage();
+
+    updateText.innerText = todoValue.value;
+    addUpdate.setAttribute("onclick", "CreateToDoItems()");
+    addUpdate.setAttribute("src", "/images/plus.png");
+    todoValue.value = "";
+    setAlertMessage("Todo item Updated Successfully!");
+}
+function DeleteToDoItems(e) {
+    let deleteValue =
+        e.parentElement.parentElement.querySelector("div").innerText;
+
+    if (confirm(`Are you sure. Due you want to delete this ${deleteValue}!`)) {
+        e.parentElement.parentElement.setAttribute("class", "deleted-item");
+        todoValue.focus();
+
+        todo.forEach((element) => {
+            if (element.item == deleteValue.trim()) {
+                todo.splice(element, 1);
+            }
+        });
+
+        setTimeout(() => {
+            e.parentElement.parentElement.remove();
+        }, 1000);
+
+        setLocalStorage();
+    }
+}
+function CompletedToDoItems(e) {
+    if (e.parentElement.querySelector("div").style.textDecoration === "") {
+        const img = document.createElement("img");
+        img.src = "/images/check-mark.png";
+        img.className = "todo-controls";
+        e.parentElement.querySelector("div").style.textDecoration = "line-through";
+        e.parentElement.querySelector("div").appendChild(img);
+        e.parentElement.querySelector("img.edit").remove();
+
+        todo.forEach((element) => {
+            if (
+                e.parentElement.querySelector("div").innerText.trim() == element.item
+            ) {
+                element.status = true;
+            }
+        });
+        setLocalStorage();
+        setAlertMessage("Todo item Completed Successfully!");
+    }
+}
+
+function setLocalStorage() {
+    localStorage.setItem("todo-list", JSON.stringify(todo));
+}
